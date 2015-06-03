2e98d95eb249640b8cf543174c3ef23cae445837
Daniel <daniel.scherzer@tum.de>
2015-06-03 15:00:25 +0200

    reload

--- src/main/resources/static/training.js
+++ src/main/resources/static/training.js
@@ -2,15 +2,6 @@ var training = angular.module('training', ['ui.bootstrap']);
 
 training.controller('TrainingController', function ($scope, $http) {
 
-    $scope.getMovies = function () {
-        $http.get('/api/movies').success(function (data) {
-            $scope.movies = data;
-        }).error(function (data, status) {
-            window.alert('Status ' + status + ': ' + data.message);
-        });
-    };
-    //$scope.getMovies();
-
     $scope.reloadMovies = function () {
         var parameter = document.getElementById('eingabe').value;
 
@@ -21,17 +12,8 @@ training.controller('TrainingController', function ($scope, $http) {
         });
     };
 
-    $scope.getSongs = function () {
-        $http.get('/api/songs').success(function (data) {
-            $scope.songs = data;
-        }).error(function (data, status) {
-            window.alert('Status ' + status + ': ' + data.message);
-        });
-    };
-    //$scope.getSongs();
-
     $scope.reloadSongs = function () {
-        var parameter2 = document.getElementById('input2').value;
+        var parameter2 = document.getElementById('eingabe').value;
 
         $http.get('/api/songs?titel=' + parameter2).success(function (data) {
             $scope.songs = data;
@@ -40,17 +22,8 @@ training.controller('TrainingController', function ($scope, $http) {
         });
     };
 
-    $scope.getActors = function () {
-        $http.get('/api/actors').success(function (data) {
-            $scope.actors = data;
-        }).error(function (data, status) {
-            window.alert('Status ' + status + ': ' + data.message);
-        });
-    };
-    //$scope.getActors();
-
     $scope.reloadActors = function () {
-        var parameter3 = document.getElementById('input3').value;
+        var parameter3 = document.getElementById('eingabe').value;
 
         $http.get('/api/actors?film=' + parameter3).success(function (data) {
             $scope.actors = data;
