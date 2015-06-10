0133de480a161a4f7f2178a092e689e06b57bc12
Daniel Scherzer <ga82zol@mytum.de>
2015-06-10 14:47:57 +0200

    ticket18

--- src/main/resources/static/training.js
+++ src/main/resources/static/training.js
@@ -10,34 +10,73 @@ training.controller('TrainingController', function ($scope, $http) {
         }).error(function (data, status) {
             window.alert('Status ' + status + ': ' + data.message);
         });
+        $scope.updateSong();
     };
 
     $scope.updateMovie = function() {
-      $scope.loadMovie($scope.film);
+        $scope.loadMovie($scope.film);
     };
 
-    $scope.reloadSongs = function () {
-        var parameter2 = document.getElementById('eingabe').value;
+    $scope.loadSong = function (film) {
+        //var parameter2 = document.getElementById('eingabe').value;
 
-        $http.get('/api/songs?titel=' + parameter2).success(function (data) {
-            $scope.songs = data;
+        $http.get('/api/song?name=' + film).success(function (data) {
+            $scope.song = data;
         }).error(function (data, status) {
             window.alert('Status ' + status + ': ' + data.message);
         });
+        $scope.updateActor();
     };
 
-    $scope.reloadActors = function () {
-        var parameter3 = document.getElementById('eingabe').value;
+    $scope.updateSong = function() {
+        $scope.loadSong($scope.film);
+    };
 
-        $http.get('/api/actors?film=' + parameter3).success(function (data) {
-            $scope.actors = data;
+    $scope.loadActor = function (film) {
+        //var parameter3 = document.getElementById('eingabe').value;
+
+        $http.get('/api/actor?name=' + film).success(function (data) {
+            $scope.actor = data;
         }).error(function (data, status) {
             window.alert('Status ' + status + ': ' + data.message);
         });
+        $scope.updateAll().delay(3000);
     };
+
+    $scope.updateActor = function() {
+        $scope.loadActor($scope.film);
+    };
+
+    $scope.updateAll = function() {
+        if ($scope.movie.uri===null){document.getElementById("tableMovies").style.display = "none";}else{document.getElementById("tableMovies").style.display = "inline";};
+        if ($scope.actor.uri===null){document.getElementById("tableActors").style.display = "none";}else{document.getElementById("tableActors").style.display = "inline";};
+        if ($scope.song.uri===null){document.getElementById("tableSongs").style.display = "none";}else{document.getElementById("tableSongs").style.display = "inline";};
+    };
+
+    /* $scope.myFunction = function() {
+     if (movie.uri === null) {
+     document.getElementById("tableMovies").style.display = "none";
+     }
+     if ($scope.actor.uri === null) {
+     document.getElementById("tableActors").style.display = "none";
+     }
+     if (song.uri === null) {
+     document.getElementById("tableSongs").style.display = "none";
+     }
+     };*/
+
     $scope.loadLatestMovies = function () {
+        /*TO DO
+         Infos via crawler Abrufen und Antwortstring generieren.
+         $scope.news="Dummy Element für viele tolle Filme";
 
-
+         $scope.latestMovies = 'default';
+         $http.get('/api/latest').success(function (data) {
+         $scope.latestMovies= data;
+         }).error(function (data, status) {
+         window.alert('Status ' + status + ': ' + data.message);
+         });*/
+        $scope.latestMovies="Loading latest movies...";
         $http.get('/api/latest').success(function (data){$scope.latestMovies = data;})
             .error(function (data, status){window.alert('Status '+ status);
             });
